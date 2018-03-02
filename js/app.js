f2e4180aef228eed3f6cf094121d1d4bf954b4a4
Grzegorz Szermiński <szerman@poczta.fm>
2018-03-02 09:55:14 +0100

    Add and modify content in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,38 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '2889',
+	'X-Auth-Token': 'cfd3b4349ccca0d3234661107be3980b'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(cardObj);
+	})
+}
+
 // OGÓLNA FUNKCJA
+/*
 function randomString() {
 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
 	var str = '', i;
@@ -7,6 +41,7 @@ function randomString() {
 	}
 	return str;
 }
+*/
 
 // TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
 var todoColumn = new Column('Do zrobienia');
